fc55dab8d4c30e5fdf49f0f4c7aab268c69b26b0
DrErvin <ervin.dragovic@gmail.com>
2024-03-31 20:29:24 +0200

    Finished 006

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,5 +1,14 @@
 'use strict';
-console.log(document.querySelector('.message').textContent);
+/*console.log(document.querySelector('.message').textContent);
 
 document.querySelector('.message').textContent = 'Pocni pogadati';
 document.querySelector('.guess').value = 2;
+*/
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = document.querySelector('.guess').value;
+  console.log(guess, typeof guess);
+
+  if (!guess) {
+    document.querySelector('.message').textContent = 'No number';
+  }
+});
