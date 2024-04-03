51547d60acbe5527d89c2482a176f708f8a23e34
DrErvin <ervin.dragovic@gmail.com>
2024-04-03 22:06:11 +0200

    Finished 014 video. End of project 2

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -1,1 +1,33 @@
 'use strict';
+const btnsOpenModal = document.querySelectorAll('.show-modal');
+const btnCloseModal = document.querySelector('.close-modal');
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+
+console.log(btnsOpenModal);
+
+const openModal = function () {
+  console.log('Button Clicked');
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+for (let i = 0; i < btnsOpenModal.length; i++) {
+  btnsOpenModal[i].addEventListener('click', openModal);
+}
+
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', function (event) {
+  if (
+    event.key === 'Escape' &&
+    !modal.classList.contains('hidden')
+  ) {
+    closeModal();
+  }
+});
