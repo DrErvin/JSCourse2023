1db390fffd0dbde3b6339207ca35efc10d4c7ee7
DrErvin <ervin.dragovic@gmail.com>
2024-06-18 18:50:03 +0200

    Finished 009 Hoisting in practice

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -1,5 +1,5 @@
 'use strict';
-
+/*
 function calcAge(birthYear) {
   const age = 2024 - birthYear;
   function printAge() {
@@ -12,3 +12,31 @@ function calcAge(birthYear) {
 
 const firstName = 'Jonas';
 calcAge(1991);
+*/
+//console.log(me);
+//console.log(job);
+//console.log(year);
+/*
+var me = 'Ervin';
+let job = 'Teacher';
+const year = 2000;
+
+console.log(addDecl(2, 3));
+console.log(addArrow);
+console.log(addExpr);
+
+function addDecl(a, b) {
+  return a + b;
+}
+
+const addExpr = function (a, b) {
+  return a + b;
+};
+
+var addArrow = (a, b) => a + b;
+*/
+//example
+
+var x = 1;
+let y = 2;
+const z = 3;
