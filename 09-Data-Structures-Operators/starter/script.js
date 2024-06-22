989ac2251a041f20d1f9deee32079a3f16af0b90
DrErvin <ervin.dragovic@gmail.com>
2024-06-22 18:10:03 +0200

    Finished challenge  #1

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -5,44 +5,44 @@ const flights =
   '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
 
 // Data needed for first part of the section
-const restaurant = {
-  name: 'Classico Italiano',
-  location: 'Via Angelo Tavanti 23, Firenze, Italy',
-  categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
-  starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
-  mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+// const restaurant = {
+//   name: 'Classico Italiano',
+//   location: 'Via Angelo Tavanti 23, Firenze, Italy',
+//   categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
+//   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
+//   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
 
-  openingHours: {
-    thu: {
-      open: 12,
-      close: 22,
-    },
-    fri: {
-      open: 11,
-      close: 23,
-    },
-    sat: {
-      open: 0, // Open 24 hours
-      close: 24,
-    },
-  },
-  orderDelivery: function ({ time, address, mainIndex, starterIndex }) {
-    console.log(`Order recieved! ${this.starterMenu[starterIndex]} and
-      ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`);
-  },
+//   openingHours: {
+//     thu: {
+//       open: 12,
+//       close: 22,
+//     },
+//     fri: {
+//       open: 11,
+//       close: 23,
+//     },
+//     sat: {
+//       open: 0, // Open 24 hours
+//       close: 24,
+//     },
+//   },
+//   orderDelivery: function ({ time, address, mainIndex, starterIndex }) {
+//     console.log(`Order recieved! ${this.starterMenu[starterIndex]} and
+//       ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`);
+//   },
 
-  orderPasta: function (ing1, ing2, ing3) {
-    console.log(`Here is your pasta with ${ing1}, ${ing2} and ${ing3}`);
-  },
-};
+//   orderPasta: function (ing1, ing2, ing3) {
+//     console.log(`Here is your pasta with ${ing1}, ${ing2} and ${ing3}`);
+//   },
+// };
 
-const ingridients = [
-  prompt('Your first ingridient: '),
-  prompt('Your second ingridient: '),
-  prompt('Your third ingridient: '),
-];
+// const ingridients = [
+//   prompt('Your first ingridient: '),
+//   prompt('Your second ingridient: '),
+//   prompt('Your third ingridient: '),
+// ];
 
-restaurant.orderPasta(...ingridients);
+// restaurant.orderPasta(...ingridients);
 
 // restaurant.orderDelivery({
 //   time: '22:3',
@@ -53,3 +53,78 @@ restaurant.orderPasta(...ingridients);
 
 // const { name, openingHours, categories } = restaurant;
 // console.log(name, openingHours, categories);
+
+//CODING CHALLANGE #1
+
+const game = {
+  team1: 'Bayern Munich',
+  team2: 'Borrussia Dortmund',
+  players: [
+    [
+      'Neuer',
+      'Pavard',
+      'Martinez',
+      'Alaba',
+      'Davies',
+      'Kimmich',
+      'Goretzka',
+      'Coman',
+      'Muller',
+      'Gnarby',
+      'Lewandowski',
+    ],
+    [
+      'Burki',
+      'Schulz',
+      'Hummels',
+      'Akanji',
+      'Hakimi',
+      'Weigl',
+      'Witsel',
+      'Hazard',
+      'Brandt',
+      'Sancho',
+      'Gotze',
+    ],
+  ],
+  score: '4:0',
+  scored: ['Lewandowski', 'Gnarby', 'Lewandowski', 'Hummels'],
+  date: 'Nov 9th, 2037',
+  odds: {
+    team1: 1.33,
+    x: 3.25,
+    team2: 6.5,
+  },
+};
+
+const [players1, players2] = game.players;
+console.log(players1);
+console.log(players2);
+
+const [gk, ...fieldPlayers] = players1;
+console.log(gk);
+console.log(fieldPlayers);
+
+const allPlayers = [...players1, ...players2];
+console.log(allPlayers);
+
+const players1Final = [...players1, 'Thiago', 'Coutinho', 'Perisic'];
+console.log(players1Final);
+
+const { team1, x: draw, team2 } = game.odds;
+console.log(team1);
+console.log(draw);
+console.log(team2);
+
+const printGoals = function (...players) {
+  console.log(
+    `${players.length} goals were scored by these players: ${players}`
+  );
+};
+
+// printGoals('Davies', 'Muller', 'Lewandowski', 'Kimmich');
+printGoals(...game.scored);
+
+// 7.
+team1 > team2 && console.log('Team 2 is more likely to win');
+team1 < team2 && console.log('Team 1 is more likely to win');
