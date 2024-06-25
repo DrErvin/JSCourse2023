db62d0cf0bc5ea9ce2b86b41d28aa5dd24d206bb
DrErvin <ervin.dragovic@gmail.com>
2024-06-25 13:04:19 +0200

    Finished 019 summary

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -215,20 +215,47 @@ const game = {
 
 // console.log(new Set('Shmedtmann').size);
 
-const rest = new Map();
-rest
-  .set('name', 'Classico Italiano')
-  .set(1, 'Firenze, Italy')
-  .set('categories', ['Italian', 'Pizzeria', 'Vegeterian'])
-  .set('open', 11)
-  .set('close', 23)
-  .set(true, 'We are open')
-  .set(false, 'We are closed');
+// const rest = new Map();
+// rest
+//   .set('name', 'Classico Italiano')
+//   .set(1, 'Firenze, Italy')
+//   .set('categories', ['Italian', 'Pizzeria', 'Vegeterian'])
+//   .set('open', 11)
+//   .set('close', 23)
+//   .set(true, 'We are open')
+//   .set(false, 'We are closed');
 
-console.log(rest);
+// console.log(rest);
 
-console.log(rest.get(1));
-const head = document.querySelector('h1');
-rest.set(head, 'Heading');
-console.log(rest);
-console.log(rest.get(head));
+// console.log(rest.get(1));
+// const head = document.querySelector('h1');
+// rest.set(head, 'Heading');
+// console.log(rest);
+// console.log(rest.get(head));
+
+const question = new Map([
+  ['question', 'What is the best programming language in the world?'],
+  [1, 'C'],
+  [2, 'Java'],
+  [3, 'JavaScript'],
+  ['correct', 3],
+  [true, 'Correct 🎉'],
+  [false, 'Try again!'],
+]);
+console.log(question);
+
+// Convert object to map
+// console.log(Object.entries(openingHours));
+// const hoursMap = new Map(Object.entries(openingHours));
+// console.log(hoursMap);
+
+// Quiz app
+console.log(question.get('question'));
+for (const [key, value] of question) {
+  if (typeof key === 'number') console.log(`Answer ${key}: ${value}`);
+}
+// const answer = Number(prompt('Your answer'));
+const answer = 3;
+console.log(answer);
+
+console.log(question.get(question.get('correct') === answer));
