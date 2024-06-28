33bf0c23cfc5028f808059dc83a68d17a413c64b
DrErvin <ervin.dragovic@gmail.com>
2024-06-28 19:51:44 +0200

    Finished 006 Callback functions

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -14,7 +14,7 @@ const createBooking = function (
   console.log(booking);
   bookings.push(booking);
 };
-createBooking('LH123');
+// createBooking('LH123');
 
 const flight = 'LH234';
 const jonas = {
@@ -32,6 +32,26 @@ const checkIn = function (flightNum, passenger) {
   //     alert('Wrong Passport!');
   //   }
 };
-checkIn(flight, jonas);
-console.log(flight);
-console.log(jonas);
+// checkIn(flight, jonas);
+// console.log(flight);
+// console.log(jonas);
+
+// CALLBACK Functions
+const oneWord = function (str) {
+  return str.replaceAll(' ', '').toLowerCase(); //REGEXP (/ /g,"");
+};
+
+const upperFirstWord = function (str) {
+  const [first, ...others] = str.split(' ');
+  return [first.toUpperCase(), ...others].join(' ');
+};
+
+const transformer = function (str, fn) {
+  console.log(`Original string: ${str}`);
+  console.log(`Transformed string: ${fn(str)}`);
+
+  console.log(`Transformed by: ${fn.name}`);
+};
+
+transformer('Javascript is the best!', upperFirstWord);
+transformer('Javascript is the best!', oneWord);
