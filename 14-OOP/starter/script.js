298e3bd69c930a4f56d9b188ee9da621125eba49
DrErvin <ervin.dragovic@gmail.com>
2024-09-04 22:12:49 +0200

    Finished 005 in OOP

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -1,1 +1,15 @@
 'use strict';
+
+const Person = function (firstName, birthYear) {
+  // instance properties
+  this.ime = firstName;
+  this.godiste = birthYear;
+};
+
+const jonas = new Person('Jonas', 1991);
+console.log(jonas);
+
+// 1. New empty object {} is created
+// 2. Function is called, this keyword is set to the newly created object this = {}
+// 3. This newly created object {} is linked to prototype
+// 4. function automatically returns that new created object
