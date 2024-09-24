eb56930ed4c3b84686f4a723a58d7117df680450
DrErvin <ervin.dragovic@gmail.com>
2024-09-24 18:38:43 +0200

    Finished 005

---
+++ 17-Modern-JS-Modules-Tooling/starter/shoppingCart.js
@@ -0,0 +1,22 @@
+// Exporting module
+console.log('Exoprting module');
+
+const shippingCost = 10;
+const cart = [];
+
+export const addToCart = function (product, quantity) {
+  cart.push({ product, quantity });
+  console.log(`${quantity} ${product} added to cart`);
+  console.log('This is starter file');
+};
+
+const totalPrice = 237;
+const totalQuantity = 23;
+
+export { totalPrice, totalQuantity as tq };
+
+export default function (product, quantity) {
+  cart.push({ product, quantity });
+  console.log(`${quantity} ${product} added to cart`);
+  console.log('This is starter file');
+}
