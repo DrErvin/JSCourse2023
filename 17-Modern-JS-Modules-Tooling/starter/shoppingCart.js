198f821c413c3015e4d2ed80e0064b2fcf9c78ee
DrErvin <ervin.dragovic@gmail.com>
2024-09-27 15:51:31 +0200

    Finished 09

--- 17-Modern-JS-Modules-Tooling/starter/shoppingCart.js
+++ 17-Modern-JS-Modules-Tooling/starter/shoppingCart.js
@@ -1,6 +1,15 @@
 // Exporting module
 console.log('Exoprting module');
 
+// Blocking code (This is the modul that is imported by script.js,
+// when you have blocking code here it will wait for the await to finish in
+// a blocking way for this file and for the file that did the import,
+// code will not start until the await promise is resovled in both files
+// this way)
+// console.log('Start fetching');
+// await fetch('https://jsonplaceholder.typicode.com/users');
+// console.log('Finish fetching users');
+
 const shippingCost = 10;
 const cart = [];
 
