93f7da88b3c9bfc6d57d07723a4f3ab8a3954eeb
DrErvin <ervin.dragovic@gmail.com>
2024-10-05 12:49:22 +0200

    Finished 012

---
+++ 18-forkify/src/js/helpers.js
@@ -0,0 +1,27 @@
+import { TIMEOUT_SEC } from './config';
+
+const timeout = function (s) {
+  return new Promise(function (_, reject) {
+    setTimeout(function () {
+      reject(new Error(`Request took too long! Timeout after ${s} second`));
+    }, s * 1000);
+  });
+};
+
+export const getJSON = async function (url) {
+  try {
+    const res = await Promise.race([
+      fetch(
+        // 'https://forkify-api.herokuapp.com/api/v2/recipes/664c8f193e7aa067e94e863d'
+        url
+      ),
+      timeout(TIMEOUT_SEC),
+    ]);
+    const data = await res.json();
+
+    if (!res.ok) throw new Error(`${data.message} (${res.status})`);
+    return data;
+  } catch (err) {
+    throw err;
+  }
+};
