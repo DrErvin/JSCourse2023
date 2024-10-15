b610d4f62181759f414b2cd0403a917053d06557
DrErvin <ervin.dragovic@gmail.com>
2024-10-15 11:38:10 +0200

    Added files for 18-forkify-new

---
+++ 18-forkify-new/src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'd61ded1e-137f-4db9-853b-5127afbf5e97';
+export const MODAL_CLOSE_SEC = 2.5;
